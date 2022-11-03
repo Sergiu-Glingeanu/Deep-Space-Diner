c884227714dbdfffde45608ab5b7b19c64b4b0ce
Sergiu Glingeanu <s.glingeanu+1@gmail.com>
2022-11-03 22:02:33 +0000

    Better drag from shop

--- Assets/Scripts/Drag_From_Shop.cs
+++ Assets/Scripts/Drag_From_Shop.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Drag_From_Shop : MonoBehaviour
+public class Drag_From_Shop : MonoBehaviour, IPointerDownHandler
 {
 
     public bool seedBag;
 
     public GameObject ui, item;
-    public Button button;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && Vector2.Distance(Input.mousePosition, transform.position) < 30f)
-        {
-            GameObject temp = Instantiate(item, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
-            temp.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-            temp.GetComponent<SeedBag>().ui = ui;
-            ui.SetActive(false);
-        }
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameObject temp = Instantiate(item, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+        temp.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+        temp.GetComponent<SeedBag>().ui = ui;
+        ui.SetActive(false);
     }
 }
